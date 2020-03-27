6875f8fe716c82132ca5c68a54242a085fe09c29
rikishikura <60309601+rikishikura@users.noreply.github.com>
2020-03-27 17:04:45 +0900

    非同期化

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -59,5 +59,8 @@ $('#new_message').on('submit', function(e){
     $('form')[0].reset();
     $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
   })
+  .fail(function() {
+    alert("メッセージ送信に失敗しました");
+});
 })
 });
